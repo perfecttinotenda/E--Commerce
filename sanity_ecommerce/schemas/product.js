0d4d76b709dba73136c35066146d3638cdbe1f0d
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2022-07-10 23:57:15 +0100

    first commit

---
+++ sanity_ecommerce/schemas/product.js
@@ -0,0 +1,40 @@
+export default {
+  name: "product",
+  title: "Pruduct",
+  type: "document",
+  fields: [
+    {
+      name: "image",
+      type: "Image",
+      type: "array",
+      of: [{ type: "image" }],
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "name",
+      type: "Name",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 90,
+      },
+    },
+    {
+      name: "price",
+      title: "Price",
+      type: "number",
+    },
+    {
+      name: "details",
+      title: "Details",
+      type: "string",
+    },
+  ],
+};
